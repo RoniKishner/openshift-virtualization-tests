9bef80637fc11117aaa0990ba48a33ade9ce7746
rkishner <rkishner@redhat.com>
2025-09-29 14:22:45 +0300

    Fix instancetype hotplug test migration restraints
    Signed-off-by: Roni Kishner <rkishner@redhat.com>

--- tests/infrastructure/instance_types/test_cpu_memory_hotplug_instancetype.py
+++ tests/infrastructure/instance_types/test_cpu_memory_hotplug_instancetype.py
@@ -25,7 +25,7 @@ from utilities.virt import (
     running_vm,
 )
 
-pytestmark = pytest.mark.usefixtures("skip_if_no_common_modern_cpu", "skip_access_mode_rwo_scope_module")
+pytestmark = [pytest.mark.usefixtures("skip_if_no_common_modern_cpu"), pytest.mark.rwx_default_storage]
 
 TESTS_CLASS_NAME = "TestCPUHotPlugInstancetype"
 
